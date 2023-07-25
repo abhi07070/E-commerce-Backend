81ba5ec4a296216fb78374b33c2a76c7dc28e078
Davinder Kumar <abhiverma97819@gmail.com>
2023-07-25 12:14:08 +0530

    added files

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  registerController,
+  loginController,
+  testController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 // router object
 const router = express.Router();
 
 //routing
 //Register || method post
-
 router.post("/register", registerController);
+
+//Login
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
 
 export default router;
